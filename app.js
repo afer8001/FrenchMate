4807c10f71fc9bb080b156ffcf10c5a5f994878b
afer8001 <ashkanafereydon@gmail.com>
2026-07-16 13:39:08 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,94 @@
+function signup(){
 
+    const username =
+    document.getElementById("username").value;
+
+    const password =
+    document.getElementById("password").value;
+
+    if(
+        username === "" ||
+        password === ""
+    ){
+        alert("Fill all fields");
+        return;
+    }
+
+    localStorage.setItem(
+        "fm_username",
+        username
+    );
+
+    localStorage.setItem(
+        "fm_password",
+        password
+    );
+
+    alert("Account Created");
+
+    window.location.href =
+    "index.html";
+
+}
+
+
+function login(){
+
+    const username =
+    document.getElementById("username").value;
+
+    const password =
+    document.getElementById("password").value;
+
+    const savedUser =
+    localStorage.getItem(
+        "fm_username"
+    );
+
+    const savedPass =
+    localStorage.getItem(
+        "fm_password"
+    );
+
+    if(
+        username === savedUser &&
+        password === savedPass
+    ){
+
+        localStorage.setItem(
+            "fm_logged",
+            "true"
+        );
+
+        window.location.href =
+        "dashboard.html";
+
+    }
+    else{
+
+        alert(
+            "Wrong Username Or Password"
+        );
+
+    }
+
+}
+
+
+if(
+window.location.pathname.includes(
+"dashboard"
+)
+){
+
+    const user =
+    localStorage.getItem(
+        "fm_username"
+    );
+
+    document.getElementById(
+        "welcome"
+    ).textContent =
+    "Welcome " + user;
+
+}
