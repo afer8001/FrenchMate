c4448dfb24cf19566739566a7a9eb1f2a74820a0
afer8001 <ashkanafereydon@gmail.com>
2026-07-16 14:35:58 +0330

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,1 +1,40 @@
+const words = {
 
+bonjour : "سلام",
+
+chat : "گربه",
+
+chien : "سگ",
+
+manger : "خوردن",
+
+aller : "رفتن"
+
+};
+
+function searchWord(){
+
+const word =
+document
+.getElementById("searchWord")
+.value
+.toLowerCase();
+
+const result =
+document
+.getElementById("result");
+
+if(words[word]){
+
+result.textContent =
+words[word];
+
+}
+else{
+
+result.textContent =
+"Word Not Found";
+
+}
+
+}
