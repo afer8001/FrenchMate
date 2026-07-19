c1275118bdda51864ed633ac789a31f955f0c76d
afer8001 <ashkanafereydon@gmail.com>
2026-07-19 17:44:43 +0330

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,1 +1,36 @@
+function renderWord(found){
 
+if(!found){
+
+document.getElementById("content").innerHTML = `
+<h2>Word Not Found</h2>
+`;
+
+return;
+}
+
+document.getElementById("content").innerHTML = `
+
+<div class="header-card">
+
+<h1>
+${found.word}
+</h1>
+
+<div class="header-fa">
+${found.meaning || "-"}
+</div>
+
+<div class="header-en">
+${found.english || "-"}
+</div>
+
+<div class="header-pronunciation">
+${found.pronunciation || "-"}
+</div>
+
+</div>
+
+`;
+
+}
