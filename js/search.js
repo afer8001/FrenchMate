b51c30f99eb9b8ff91683f5a303bd16bac8a9eee
afer8001 <ashkanafereydon@gmail.com>
2026-07-19 17:43:55 +0330

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,1 +1,19 @@
+async function searchWord(word){
 
+if(!word) return null;
+
+const firstLetter =
+word[0].toLowerCase();
+
+const words =
+await loadLetter(firstLetter);
+
+const found =
+words.find(
+item =>
+item.word.toLowerCase() === word.toLowerCase()
+);
+
+return found || null;
+
+}
