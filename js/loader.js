95ed004b2e4aa2f9e1399378560aea5e685de6f4
afer8001 <ashkanafereydon@gmail.com>
2026-07-19 17:42:11 +0330

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,32 @@
+const loadedLetters = {};
 
+async function loadLetter(letter){
+
+letter = letter.toLowerCase();
+
+if(loadedLetters[letter]){
+return loadedLetters[letter];
+}
+
+try{
+
+const response =
+await fetch(`data/${letter}.json`);
+
+const data =
+await response.json();
+
+loadedLetters[letter] = data;
+
+return data;
+
+}
+catch(error){
+
+console.error(error);
+
+return [];
+
+}
+
+}
