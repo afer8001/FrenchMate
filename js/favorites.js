b220d3dcd8424571aa816521766b44643ee9817d
afer8001 <ashkanafereydon@gmail.com>
2026-07-20 18:39:49 +0330

    Update favorites.js

--- js/favorites.js
+++ js/favorites.js
@@ -1,1 +1,85 @@
+let favorites =
+JSON.parse(
+localStorage.getItem("favorites")
+) || [];
 
+function isFavorite(word){
+
+return favorites.includes(word);
+
+}
+
+function toggleFavorite(word){
+
+if(isFavorite(word)){
+
+favorites =
+favorites.filter(
+item => item !== word
+);
+
+}else{
+
+favorites.push(word);
+
+}
+
+localStorage.setItem(
+"favorites",
+JSON.stringify(favorites)
+);
+
+}
+
+function showFavorites(){
+
+let html = `
+
+<div class="header-card">
+
+<h1>⭐ Favorites</h1>
+
+</div>
+
+`;
+
+if(favorites.length === 0){
+
+html += `
+
+<div class="section-card">
+
+<p>No favorite words yet.</p>
+
+</div>
+
+`;
+
+}else{
+
+favorites.forEach(word=>{
+
+html += `
+
+<div class="section-card">
+
+<a href="#"
+onclick="openWord('${word}')">
+
+${word}
+
+</a>
+
+</div>
+
+`;
+
+});
+
+}
+
+document.getElementById(
+"content"
+).innerHTML = html;
+
+}
